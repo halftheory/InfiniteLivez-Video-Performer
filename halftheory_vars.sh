5902b6a3e2a4602e85c48a4f1efb93c27eba385a
halftheory <mail@halftheory.com>
2022-02-05 21:39:08 +0100

    update

--- halftheory_vars.sh
+++ halftheory_vars.sh
@@ -28,65 +28,8 @@ CHMOD_DIRS="755"
 CHMOD_FILES="644"
 CHMOD_UPLOADS="777"
 DIR_SCRIPTS="/usr/local/bin"
-FILE_CONFIG="/boot/config.txt"
-FILE_RCLOCAL="/etc/rc.local"
 
 if [ ! -d "$DIR_DATA" ]; then
 	mkdir -p $DIR_DATA
 	chmod $CHMOD_DIRS $DIR_DATA
 fi
-
-# functions
-
-function is_opengl_legacy()
-{
-	if file_contains_line "$FILE_CONFIG" "dtoverlay=vc4-kms-v3d" || file_contains_line "$FILE_CONFIG" "dtoverlay=vc4-kms-v3d-pi4"; then
-		return 1
-	fi
-	return 0
-}
-
-function is_vcgencmd_working()
-{
-	STR_TEST="$(get_os_version_id)"
-	if is_which "vcgencmd" && is_int "$STR_TEST" && (($STR_TEST < 11)); then
-		return 0
-	fi
-	return 1
-}
-
-function file_add_line_config_after_all()
-{
-	# STRING
-	if [ -z "$1" ]; then
-		return 1
-	fi
-	if ! file_contains_line "$FILE_CONFIG" "$*"; then
-		if file_contains_line "$FILE_CONFIG" "#$*"; then
-			file_add_line "$FILE_CONFIG" "$*" "sudo"
-		elif ! file_replace_line_first "$FILE_CONFIG" "(\[all\])" "\1\n$*" "sudo"; then
-			file_add_line "$FILE_CONFIG" "$*" "sudo"
-		fi
-	else
-		return 1
-	fi
-	return 0
-}
-
-function file_add_line_rclocal_before_exit()
-{
-	# STRING
-	if [ -z "$1" ]; then
-		return 1
-	fi
-	if ! file_contains_line "$FILE_RCLOCAL" "$*"; then
-		if file_contains_line "$FILE_RCLOCAL" "#$*"; then
-			file_add_line "$FILE_RCLOCAL" "$*" "sudo"
-		elif ! file_replace_line_first "$FILE_RCLOCAL" "(exit 0)" "$*\n\1" "sudo"; then
-			file_add_line "$FILE_RCLOCAL" "$*" "sudo"
-		fi
-	else
-		return 1
-	fi
-	return 0
-}
