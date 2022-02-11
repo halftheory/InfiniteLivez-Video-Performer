0a52cc6bb8f6ed53932d8f1735d51100156abeb0
Half/theory <noreply@halftheory.com>
2022-02-11 18:37:43 +0100

    update

--- halftheory_vars.sh
+++ halftheory_vars.sh
@@ -15,15 +15,67 @@ else
 fi
 
 # vars
+DIRNAME="$(get_realpath "$DIRNAME")"
 MAYBE_SUDO="$(maybe_sudo)"
+OWN_LOCAL="$(whoami)"
+GRP_LOCAL="$(get_file_grp "$0")"
+DIR_LOCAL="$(get_user_dir "$OWN_LOCAL")"
 CHMOD_DIRS="755"
 CHMOD_FILES="644"
 DIR_SCRIPTS="/usr/local/bin"
+FILE_CONFIG="/boot/config.txt"
+FILE_RCLOCAL="/etc/rc.local"
 
-DIRNAME="$(get_realpath "$DIRNAME")"
-DIR_DATA="$DIRNAME/data"
-DIR_MEDIA="$DIR_DATA/media"
-SH_MIDIDUMP="$DIRNAME/mididump.sh"
-FILE_BEAT="$DIR_DATA/beat.txt"
-FILE_MIDIDUMP="$DIR_DATA/mididump.txt"
-FILE_SETTINGS="$DIR_DATA/settings.txt"
+# functions
+function is_opengl_legacy()
+{
+	if file_contains_line "$FILE_CONFIG" "dtoverlay=vc4-kms-v3d" || file_contains_line "$FILE_CONFIG" "dtoverlay=vc4-kms-v3d-pi4"; then
+		return 1
+	fi
+	return 0
+}
+
+function is_vcgencmd_working()
+{
+	local STR_TEST="$(get_os_version_id)"
+	if is_which "vcgencmd" && is_int "$STR_TEST" && (($STR_TEST < 11)); then
+		return 0
+	fi
+	return 1
+}
+
+function file_add_line_config_after_all()
+{
+	# STRING
+	if [ -z "$1" ]; then
+		return 1
+	fi
+	if ! file_contains_line "$FILE_CONFIG" "$*"; then
+		if file_contains_line "$FILE_CONFIG" "#$*"; then
+			file_add_line "$FILE_CONFIG" "$*" "sudo"
+		elif ! file_replace_line_first "$FILE_CONFIG" "(\[all\])" "\1\n$*" "sudo"; then
+			file_add_line "$FILE_CONFIG" "$*" "sudo"
+		fi
+	else
+		return 1
+	fi
+	return 0
+}
+
+function file_add_line_rclocal_before_exit()
+{
+	# STRING
+	if [ -z "$1" ]; then
+		return 1
+	fi
+	if ! file_contains_line "$FILE_RCLOCAL" "$*"; then
+		if file_contains_line "$FILE_RCLOCAL" "#$*"; then
+			file_add_line "$FILE_RCLOCAL" "$*" "sudo"
+		elif ! file_replace_line_first "$FILE_RCLOCAL" "(exit 0)" "$*\n\1" "sudo"; then
+			file_add_line "$FILE_RCLOCAL" "$*" "sudo"
+		fi
+	else
+		return 1
+	fi
+	return 0
+}
